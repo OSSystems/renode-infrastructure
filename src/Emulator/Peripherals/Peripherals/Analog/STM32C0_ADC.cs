c19143daa92c1836ab9cbc8be3f4e79bc8759728
Gerson Fernando Budke <gerson.budke@ossystems.com.br>
2023-12-29 10:57:28 +0100

    [GH#569] Analog: Add STM32C0 ADC driver
    Signed-off-by: Gerson Fernando Budke <gerson.budke@ossystems.com.br>
    (cherry picked from commit e28686e40cf4cfe30913ac30fdc5e5eb81108505)
    (cherry picked from commit 92acbc7909cb68a77c317849642d7e49d47786a2)
    
    Ported to Renode v1.17.0 STM32_ADC_Common API.

---
+++ src/Emulator/Peripherals/Peripherals/Analog/STM32C0_ADC.cs
@@ -0,0 +1,46 @@
+//
+// Copyright (c) 2010-2024 Antmicro
+// Copyright (c) 2023-2025 OS Systems
+//
+// This file is licensed under the MIT License.
+// Full license text is available in 'licenses/MIT.txt'.
+//
+
+using Antmicro.Renode.Core;
+using Antmicro.Renode.Peripherals.DMA;
+
+namespace Antmicro.Renode.Peripherals.Analog
+{
+    public class STM32C0_ADC : STM32_ADC_Common
+    {
+        public STM32C0_ADC(IMachine machine, double referenceVoltage, uint externalEventFrequency, int dmaChannel = 0, IDMA dmaPeripheral = null)
+            : base(
+                machine,
+                referenceVoltage,
+                externalEventFrequency,
+                dmaChannel,
+                dmaPeripheral,
+                // Base class configuration
+                watchdogCount: 3,
+                hasCalibration: true,
+                hasHighCalAddress: false,
+                channelCount: 23,
+                hasPrescaler: false,
+                hasVbatPin: false,
+                hasChannelSelect: true,
+                hasChannelSequence: true,
+                hasPowerRegister: false,
+                hasOffset: false,
+                hasDifferentialMode: false,
+                samplingTime: SamplingTime.TwoSelections,
+                dualMode: false,
+                hasLinearityCalibration: false,
+                hasChannelInjection: false,
+                hasSeparateThresholdRegisters: false,
+                resolutionRange: ResolutionRange.Bits6_12,
+                hasChannelPreselection: false,
+                hasScanDirection: true
+            )
+        { }
+    }
+}
